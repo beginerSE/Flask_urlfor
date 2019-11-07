514c8b3cd3424bc38a916dbcfb66082c74c4fc31
零細SE <42573936+beginerSE@users.noreply.github.com>
2019-11-07 10:45:37 +0900

    サンプルコードを追加

--- app.py
+++ app.py
@@ -1,21 +1,33 @@
-from flask import Flask, request, render_template 
-from datetime import datetime
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, render_template, url_for, redirect
 app = Flask(__name__)
 
+
+# トップ画面
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+# 社員一覧
 @app.route("/members")
 def user_url():
-    lists = [[1,'太郎'], [2,'次郎'], [3,'三郎']]
+    lists = [[1, '太郎'], [2, '次郎'], [3, '三郎']]
     return render_template("member.html", lists=lists)
 
 
+# 社員詳細情報
 @app.route("/member_detail/<int:id>/<name>")
-def detail(id,name):
-    lists = [['太郎',24,'サッカー'], ['次郎',45,'野球'], ['三郎',67,'バスケ']]
+def detail(id, name):
+    lists = [['太郎', 24, 'サッカー'], ['次郎', 45, '野球'], ['三郎', 67, 'バスケ']]
     index = id-1
     data = lists[index]
     return render_template("detail.html", data=data, name=name)
 
 
+# トップ画面にリダイレクト
+@app.route('/redirects')
+def redirects():
+    return redirect(url_for('index'))
 
 
+app.run(debug=True)
